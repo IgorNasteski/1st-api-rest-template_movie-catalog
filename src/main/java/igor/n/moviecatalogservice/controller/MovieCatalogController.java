bace2790304f31f251d27b9bf386d3eabffb34b6
Igor Nasteski <igor.nasteski92@gmail.com>
2020-12-24 02:35:22 +0100

    upgrading restTemplate communication

--- src/main/java/igor/n/moviecatalogservice/controller/MovieCatalogController.java
+++ src/main/java/igor/n/moviecatalogservice/controller/MovieCatalogController.java
@@ -3,11 +3,15 @@ package igor.n.moviecatalogservice.controller;
 import igor.n.moviecatalogservice.model.CatalogItem;
 import igor.n.moviecatalogservice.model.Movie;
 import igor.n.moviecatalogservice.model.Rating;
+import igor.n.moviecatalogservice.model.RatingResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieCatalog")
@@ -16,14 +20,20 @@ public class MovieCatalogController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping("/{userId}")
-    public CatalogItem getCatalogItems(@PathVariable("userId")String userId){
-        Rating rating = restTemplate.getForObject("http://localhost:8083/ratings/" + userId, Rating.class);
-        Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+    @RequestMapping("/{userId}")                        //ZBOG KORISCENJA EUREKE MENJAM IZ http://localhost:8083/ratings/users/ U http://rating-data-service/ratings/users/
+    public List<CatalogItem> getCatalogItems(@PathVariable("userId")String userId){
+        RatingResponse ratings = restTemplate.getForObject("http://rating-data-service/ratings/users/" + userId, RatingResponse.class);
 
-        CatalogItem catalogItem = new CatalogItem(movie.getName(), "opis zakucan", rating.getRating());
+        List<CatalogItem> catalogItems = new ArrayList<>();
 
-        return catalogItem;
+        for(Rating rating : ratings.getRatings()){      //ZBOG KORISCENJA EUREKE MENJAM IZ http://localhost:8082/movies/ U http://movie-info-service/movies/
+            //For each movie ID, call movie info service and get details
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+            //Put them all together
+            catalogItems.add(new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating()));
+        }
+
+        return catalogItems;
     }
 
 }
--- src/main/java/igor/n/moviecatalogservice/model/Movie.java
+++ src/main/java/igor/n/moviecatalogservice/model/Movie.java
@@ -4,12 +4,14 @@ public class Movie {
 
     private String movieId;
     private String name;
+    private String description;
 
-    public Movie() {
-    }
-    public Movie(String movieId, String name) {
+    public Movie(){}
+
+    public Movie(String movieId, String name, String description) {
         this.movieId = movieId;
         this.name = name;
+        this.description = description;
     }
 
     public String getMovieId() {
@@ -23,5 +25,11 @@ public class Movie {
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
